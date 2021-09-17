c86afdbd210409ac7343db1df88741efbd95667c
tr43om <tr43om@gmail.com>
2021-09-17 22:03:20 +0600

    half way there

---
+++ js/script.js
@@ -0,0 +1,77 @@
+let isYearly = false;
+const slider = document.querySelector(".card__slider");
+const switchLabel = document.querySelector(".card__switch + label");
+const priceContainer = document.querySelector(".card__component--price");
+
+// Formats price
+const formattedPrice = (value) =>
+  new Intl.NumberFormat(navigator.language, {
+    style: "currency",
+    currency: "USD",
+  }).format(Number(isYearly ? Number(value * 12 * 0.75) : value));
+
+// Sets initial state to slider and defaul value
+updateSlider(14);
+
+// Updates price and billing
+switchLabel.addEventListener("transitionend", (e) => {
+  if (e.propertyName !== "left") updateToggle();
+  updateSlider(slider.value);
+});
+
+// Update slider
+function updateSlider(value) {
+  const priceLabel = document.getElementById("rangeValue");
+  const viewsLabel = document.querySelector(".card__views");
+  // Display price
+  (function displayPrice() {
+    priceLabel.textContent = formattedPrice(value);
+  })();
+
+  // Slider track background
+  (function setGradient() {
+    const sliderBackground = `linear-gradient(to right, var(--soft-cyan) 0%, var(--soft-cyan) ${Math.trunc(
+      Number(value) * 3.125
+    )}%, var(--light-grayish-blue-1) ${Math.trunc(
+      Number(value) * 3.125
+    )}%, var(--light-grayish-blue-1) 100%)`;
+    slider.style.backgroundImage = sliderBackground;
+  })();
+
+  (function updateViews() {
+    viewsLabel.textContent = `${Math.trunc(Number(value) * 3.125)}k pageviews`;
+  })();
+}
+
+// Update toggle
+function updateToggle() {
+  // Display type of billing (yearly / monthly)
+  (function displayBilling() {
+    // Switches between type of billing
+    isYearly = isYearly ? false : true;
+
+    const billingLabel = document.querySelector(".card__billing");
+    const toggle = document.querySelector(".card__switch");
+    const billing = document.createElement("span");
+
+    billing.className = "card__billing";
+    billing.textContent = `${!toggle.checked ? "/ month" : "/ year"}`;
+
+    priceContainer.appendChild(billing);
+    priceContainer.replaceChild(billing, billingLabel);
+  })();
+
+  // Display price (the code written this way to fire animation)
+  (function displayPrice() {
+    const priceLabel = document.querySelector(".card__price");
+    const priceValue = slider.value;
+
+    const price = document.createElement("span");
+    price.id = "rangeValue";
+    price.className = "card__price";
+    price.textContent = formattedPrice(priceValue);
+
+    priceContainer.appendChild(price);
+    priceContainer.replaceChild(price, priceLabel);
+  })();
+}
